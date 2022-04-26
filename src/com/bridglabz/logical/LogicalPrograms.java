060dc18192c3c240791a823f3f4f62fd67405987
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-27 02:09:49 +0530

    Added: Checking if the Number is Prime or Not.

--- src/com/bridglabz/logical/LogicalPrograms.java
+++ src/com/bridglabz/logical/LogicalPrograms.java
@@ -1,24 +1,23 @@
 package com.bridglabz.logical;
-import java.util.Scanner;
 
 public class LogicalPrograms {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(" Enter a Number");
-        int number = scanner.nextInt();
-        int sum = 0;
-        int i = 1;
-
-        while (i <= number / 2) {
-            if (number % i == 0) {
-                sum = sum + i;
-            }
-            i++;
-        }
-        if (sum == number) {
-            System.out.println(number + " is a Perfect Number");
+    public static void main(String args[]) {
+        int i, m = 0, flag = 0;
+        int n = 3;
+        m = n / 2;
+        if (n == 0 || n == 1) {
+            System.out.println(n + " is not prime number");
         } else {
-            System.out.println(number + " is not a Perfect Number");
+            for (i = 2; i <= m; i++) {
+                if (n % i == 0) {
+                    System.out.println(n + " is not prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println(n + " is prime number");
+            }
         }
     }
 }
