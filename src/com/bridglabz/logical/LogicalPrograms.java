b5df086f6c74d44c2f81a260b21ffa4e6c20ee81
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-27 01:45:24 +0530

    Added: Fibonacci Series Added.

--- src/com/bridglabz/logical/LogicalPrograms.java
+++ src/com/bridglabz/logical/LogicalPrograms.java
@@ -1,7 +1,18 @@
 package com.bridglabz.logical;
 
 public class LogicalPrograms {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Logical Programs.");
+    public static void main(String args[]) {
+        int n1 = 0, n2 = 1, n3, i, count = 10;
+        System.out.print(n1 + " " + n2);
+
+        for (i = 2; i < count; ++i)
+        {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+
+        }
+
     }
 }
