ca2104fbab72ad46696c92593405f9225efc3ee1
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-27 02:03:53 +0530

    Added: Checking if the Number is Perfect or Not.

--- src/com/bridglabz/logical/LogicalPrograms.java
+++ src/com/bridglabz/logical/LogicalPrograms.java
@@ -1,18 +1,24 @@
 package com.bridglabz.logical;
+import java.util.Scanner;
 
 public class LogicalPrograms {
-    public static void main(String args[]) {
-        int n1 = 0, n2 = 1, n3, i, count = 10;
-        System.out.print(n1 + " " + n2);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter a Number");
+        int number = scanner.nextInt();
+        int sum = 0;
+        int i = 1;
 
-        for (i = 2; i < count; ++i)
-        {
-            n3 = n1 + n2;
-            System.out.print(" " + n3);
-            n1 = n2;
-            n2 = n3;
-
+        while (i <= number / 2) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+            i++;
         }
-
+        if (sum == number) {
+            System.out.println(number + " is a Perfect Number");
+        } else {
+            System.out.println(number + " is not a Perfect Number");
+        }
     }
 }
